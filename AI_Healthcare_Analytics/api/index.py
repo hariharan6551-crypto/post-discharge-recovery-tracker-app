b847d88ce37260324e2c713b0620cbfb3f7e52aa
leol77882 <leo123@gmail.com>
2026-03-11 01:48:35 +0530

    Fix Vercel routing

--- AI_Healthcare_Analytics/api/index.py
+++ AI_Healthcare_Analytics/api/index.py
@@ -1,11 +1,15 @@
 import subprocess
 
 def handler(request, response):
-    subprocess.Popen(
-        ["streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
-    )
+    subprocess.Popen([
+        "streamlit",
+        "run",
+        "app.py",
+        "--server.port=8501",
+        "--server.address=0.0.0.0"
+    ])
 
     return {
         "statusCode": 200,
-        "body": "Streamlit app started"
+        "body": "Streamlit server started"
     }
